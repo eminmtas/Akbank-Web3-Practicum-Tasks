e1187b3d54a5a16e3b33908d173b127aa2832fa8
Muhammed Emin TAŞ <eminmtas@gmail.com>
2022-09-20 23:39:12 +0300

    additional comments added

--- ERC20.sol
+++ ERC20.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// Owner: 0xbabacA9617a2cC30bC34A18E5F251F4Bd8CAbfac
+// Contract address: 0xE55238028E8079DA6e6b2cD738c2f63689F4b25b
+
 contract EminDAO is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("Emin DAO", "EDAO") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
